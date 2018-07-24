1bfff836c040bcaf58afe798a87638af4b842107
yousof <yousof777@gmail.com>
2018-07-24 17:06:40 +0200

    commit changes to file locations

---
+++ script/main.js
@@ -0,0 +1,11 @@
+let squares = 20;
+
+function makeSquares(squares) {
+  let container = document.querySelector('.grid-container');
+  for (var i = 0; i < i; i++) {
+    const square = document.createElement('div');
+    gridcontainer.appendChild(square);
+  }
+}
+
+makeSquares();
